17dbdfa1a9eaba42aff7f2b903dc20d45e6d7f86
Daniel OTSHUDIEMA <danots70@gmail.com>
2026-01-18 12:31:40 +0100

    Correctif : Ajustement de la mise en page du pied de page
    La structure du composant `footer` a été modifiée pour mieux regrouper le badge "Built with Jaspr" et le texte de copyright.
    
    Précédemment, le badge et le contenu du pied de page étaient des enfants directs du conteneur principal. Ils sont désormais encapsulés dans une `div` commune pour améliorer la cohérence de la mise en page.

--- lib/sections/footer_section.dart
+++ lib/sections/footer_section.dart
@@ -8,9 +8,11 @@ class FooterSection extends StatelessComponent {
   Component build(BuildContext context) {
     return footer([
       div(classes: 'container', [
-        a(href: 'https://jaspr.site',target: Target.blank, [JasprBadge.light()]),
         div(classes: 'footer-content', [
-          p([.text('© 2026 Developed with ❤️ using Dart & Jaspr.')]),
+          div([
+            a(href: 'https://jaspr.site', target: Target.blank, [JasprBadge.light()]),
+            p([.text('© 2026 Developed with ❤️ using Dart & Jaspr.')]),
+          ]),
           div(classes: 'social-links', [
             a(href: '#', [.text('GitHub')]),
             a(href: '#', [.text('LinkedIn')]),
