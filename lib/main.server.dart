288fe6f2473ddd2b7a36f83f9d1fa965617d6d10
Daniel OTSHUDIEMA <danots70@gmail.com>
2026-01-21 21:06:28 +0100

    ``` CI : Ajout d'un chemin de base pour le déploiement sur GitHub Pages
    Le chemin de base `https://heydanots.github.io/portfolio` a été ajouté au `Document` dans `main.server.dart` afin de garantir que les ressources soient correctement chargées lors du déploiement sur GitHub Pages.

--- lib/main.server.dart
+++ lib/main.server.dart
@@ -26,6 +26,7 @@ void main() {
   runApp(
     Document(
       title: 'HDS',
+      base: 'portfolio',
       styles: [
         css.import('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'),
         css.import('styles.css'),
